0277064789ff2e086bfb22129510b73f126689f7
ofilipesouza <filipesouza@Luizs-Air.home>
2023-06-08 02:05:53 +0100

    init

---
+++ src/test/java/dev/ofilipesouza/tasksphere/TaskSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ofilipesouza.tasksphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSphereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
